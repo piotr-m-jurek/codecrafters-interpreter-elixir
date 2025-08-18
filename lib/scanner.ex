ae13290f898f410bb5f2eafe70f5a21febb90943
Piotr Malecki-Jurek <piotr.m.jurek@checkhq.com>
2025-08-18 13:53:55 +0200

    codecrafters submit [skip ci]

--- lib/scanner.ex
+++ lib/scanner.ex
@@ -1,22 +1,52 @@
 defmodule Scanner do
-  @type token :: {name :: String.t(), sign :: String.t()}
+  @type token :: {name :: String.t(), sign :: String.t(), nil}
 
+  @spec scan(contents :: String.t()) :: [token()]
   def scan(contents), do: scan(contents, [])
 
+  @spec scan(contents :: String.t(), tokens :: [token()]) :: [token()]
   def scan("(" <> rest, tokens) do
-    scan(rest, [{"LEFT_PAREN", "("} | tokens])
+    scan(rest, [{"LEFT_PAREN", "(", nil} | tokens])
   end
 
   def scan(")" <> rest, tokens) do
-    scan(rest, [{"RIGHT_PAREN", ")"} | tokens])
+    scan(rest, [{"RIGHT_PAREN", ")", nil} | tokens])
   end
 
   def scan("{" <> rest, tokens) do
-    scan(rest, [{"LEFT_BRACE", "{"} | tokens])
+    scan(rest, [{"LEFT_BRACE", "{", nil} | tokens])
   end
 
   def scan("}" <> rest, tokens) do
-    scan(rest, [{"RIGHT_BRACE", "}"} | tokens])
+    scan(rest, [{"RIGHT_BRACE", "}", nil} | tokens])
+  end
+
+  def scan("*" <> rest, tokens) do
+    scan(rest, [{"STAR", "*", nil} | tokens])
+  end
+
+  def scan("." <> rest, tokens) do
+    scan(rest, [{"DOT", ".", nil} | tokens])
+  end
+
+  def scan("," <> rest, tokens) do
+    scan(rest, [{"COMMA", ",", nil} | tokens])
+  end
+
+  def scan("+" <> rest, tokens) do
+    scan(rest, [{"PLUS", "+", nil} | tokens])
+  end
+
+  def scan("-" <> rest, tokens) do
+    scan(rest, [{"MINUS", "-", nil} | tokens])
+  end
+
+  def scan(";" <> rest, tokens) do
+    scan(rest, [{"SEMICOLON", ";", nil} | tokens])
+  end
+
+  def scan("/" <> rest, tokens) do
+    scan(rest, [{"SLASH", "/", nil} | tokens])
   end
 
   def scan("\n" <> rest, tokens) do
@@ -24,11 +54,11 @@ defmodule Scanner do
   end
 
   def scan("", tokens) do
-    Enum.reverse([{"EOF", ""} | tokens])
+    Enum.reverse([{"EOF", "", nil} | tokens])
   end
 
   @spec print_token(token :: token()) :: binary()
-  def print_token({name, sign}) do
+  def print_token({name, sign, _}) do
     [name, sign, "null"] |> Enum.join(" ")
   end
 end
